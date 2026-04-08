10c1873b1eaaaec1e7a71587bb246ba540d70f6c
chenjie199234 <chenjie199234@outlook.com>
2026-04-08 14:08:54 +0800

    update

--- cmd.bat
+++ cmd.bat
@@ -160,7 +160,7 @@ goto :eof
 :help
 	echo cmd.bat - every thing you need
 	echo           please install git
-	echo           please install golang(1.26.1+)
+	echo           please install golang(1.26.2+)
 	echo           please install protoc           (github.com/protocolbuffers/protobuf)
 	echo           please install protoc-gen-go    (github.com/protocolbuffers/protobuf-go)
 	echo           please install codegen          (github.com/chenjie199234/Corelib)
